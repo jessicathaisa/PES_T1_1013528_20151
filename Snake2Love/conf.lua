9b68e7fa0a3ebd4f6a0882bf1f903da96013220e
Jessica Thaisa Silva de Almeida <jessicatsalmeida@gmail.com>
2015-04-15 21:02:14 -0300

    Desabilitando exibição de prints no console

--- Snake2Love/conf.lua
+++ Snake2Love/conf.lua
@@ -3,7 +3,7 @@ function love.conf(v)
 	--configure game
 	v.title = "Snake2Love"
 	v.author = "Jéssica Thaisa Almeida"
-	v.console = true
+	v.console = false
 	
 	v.window.width = 32 * 40
 	v.window.height = 32 * 20
